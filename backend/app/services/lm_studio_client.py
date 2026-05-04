17687412845b7cdff521f1f42b08b45b2b3a3195
bogdan-chis <chisbogdan.2020cbm@gmail.com>
2026-05-04 17:48:06 +0300

    Added lm studio client

---
+++ backend/app/services/lm_studio_client.py
@@ -0,0 +1,22 @@
+import httpx
+from core.config import app_settings
+
+class LMClient:
+    def __init___(self):
+        self.base_url = app_settings.BASE_URL
+        self.model = app_settings.MODEL
+        self.temperature = app_settings.TEMPERATURE
+
+    async def chat_completion(self, messages: list[dict]):
+        payload = {
+            "model":self.model,
+            "messages":messages,
+            "temperature":self.temperature
+        }
+
+        async with httpx.AsyncClient() as client:
+            response = await client.post(f"{self.base_url}/chat/completions")
+            response.raise_for_status()
+
+            data = response.json()
+            return data["choices"][0]["message"]["content"]
